4e05dea792c6af25859343b28342ba2d10d7b93c
yudafatah <yudafatah@gmail.com>
2018-12-13 10:15:15 +0700

    add helper

---
+++ src/java/helper/countDate.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author yudafatah
+ */
+public class countDate {
+    
+    public static int countDays(String startDate, String endDate) throws Exception {
+    int workingDays = 0;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    Calendar startdate = Calendar.getInstance();
+    startdate.setTime(sdf.parse(startDate));
+    Calendar enddate = Calendar.getInstance();
+    enddate.setTime(sdf.parse(endDate));
+
+    while (!startdate.after(enddate)) {
+        int day = startdate.get(Calendar.DAY_OF_WEEK);
+        System.out.println(day);
+        if ((day != Calendar.SATURDAY) && (day != Calendar.SUNDAY)) {
+            workingDays++;
+        }
+
+        // increment start date, otherwise while will give infinite loop
+        startdate.add(Calendar.DATE, 1);
+    }
+
+    return workingDays;
+}
+}
